3fb321cd90355e01cfac20ef41ddde8e4c174827
Yudi Tan <yudi98@berkeley.edu>
2019-03-22 15:06:35 -0700

    Completed vessel service, updated consignment

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -2,36 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	micro "github.com/micro/go-micro"
 	pb "github.com/yuditan/shipper/consignment-service/proto/consignment"
+	vesselProto "github.com/yuditan/shipper/vessel-service/proto/vessel"
 	"golang.org/x/net/context"
 )
 
-type IRepository interface {
+type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
-type Repository struct {
+type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
-func (repo *Repository) GetAll() []*pb.Consignment {
+func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
 type service struct {
-	repo IRepository
+	repo         Repository
+	vesselClient vesselProto.VesselServiceClient
 }
 
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+
+	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity:  int32(len(req.Containers)),
+	})
+	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
+	if err != nil {
+		return err
+	}
+
+	req.VesselId = vesselResponse.Vessel.Id
+
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return err
@@ -48,7 +63,7 @@ func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *
 }
 
 func main() {
-	repo := &Repository{}
+	repo := &ConsignmentRepository{}
 
 	srv := micro.NewService(
 		//name must match the package name in protobuf definition
@@ -56,10 +71,12 @@ func main() {
 		micro.Version("latest"),
 	)
 
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+
 	srv.Init()
 
 	//register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
 	// Run the server
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
